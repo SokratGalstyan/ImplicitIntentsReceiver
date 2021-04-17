b66ad34bc4d5e5e41dd179d7b0a999d35bcd5760
SokratGalstyan <sgalstyan1999@gmail.com>
2021-04-17 17:43:50 +0400

    Initial commit

---
+++ app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.implicitintentsreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val uri = intent.data
+
+        if (uri != null) {
+            val uriString = "${getString(R.string.uri_label)} ${uri.toString()}"
+            val textView = findViewById<TextView>(R.id.text_uri_message)
+            textView.text = uriString
+        }
+    }
+}
